02c846d5254705d1b1fcfd6c643d0595cf6dde42
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2023-01-07 17:01:54 -0600

    forkphorus
    project player

---
+++ dist/index.81fc9838.js
@@ -0,0 +1,38 @@
+var player = new P.player.Player();
+var wrapper = document.querySelector("div.player-wrapper");
+player.setOptions({
+    autoplayPolicy: "never"
+});
+player.setOptions({
+    theme: "light"
+});
+player.setOptions({
+    focusOnLoad: false
+});
+wrapper.classList.add("has-ui");
+player.addControls();
+new P.player.ProgressBar(player, {
+    position: "controls"
+});
+new P.player.ErrorHandler(player);
+document.querySelector(".player").appendChild(player.root);
+player.onprogress.subscribe(function(progress) {
+    console.log("progress:", progress);
+});
+player.onload.subscribe(function() {
+    console.log("loaded");
+});
+player.onstartload.subscribe(function() {
+    console.log("startload");
+});
+player.onerror.subscribe(function(error) {
+    console.log("error!");
+});
+player.onpause.subscribe(function(error) {
+    console.log("paused");
+});
+function loadProject(file) {
+    player.loadProjectFromBuffer(file);
+}
+
+//# sourceMappingURL=index.81fc9838.js.map
