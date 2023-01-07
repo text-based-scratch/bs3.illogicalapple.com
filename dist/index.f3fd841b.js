420086c623e19a89bf2a72475acdee07ef390a91
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2023-01-07 15:24:17 -0600

    update gui
    add more stuff, including tab thing and make editor more secure (no more contenteditable)

--- dist/index.f3fd841b.js
+++ dist/index.f3fd841b.js
@@ -1,2 +1,14 @@
+const $ = (...args)=>document.querySelector(...args);
+addEventListener("load", function() {
+    $("div.text-editor").addEventListener("click", ()=>setTimeout(()=>$("div.contenteditable").focus(), 0));
+    $("div.contenteditable").addEventListener("input", ()=>{
+        $("div.text-editor pre.text").innerText = $("div.contenteditable").innerText;
+        if ($("div.text-editor div.caret").classList.contains("animation")) {
+            $("div.text-editor div.caret").classList.remove("animation");
+            setTimeout(()=>$("div.text-editor div.caret").classList.add("animation"), 1000);
+        }
+        if ($("div.contenteditable").innerText == "") $("div.contenteditable").innerHTML = "&nbsp;";
+    });
+});
 
 //# sourceMappingURL=index.f3fd841b.js.map
